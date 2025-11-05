81c694b73adaca10bb13addf994820e7fe8e46fd
Braian Hoeltgebaum <braian_hoeltgebaum@estudante.sesisenai.org.br>
2025-11-04 21:34:33 -0300

    Produto Repository
    Adicionar ao acabar o produtoDAO

--- src/main/java/com/tecdes/pedido/repository/ProdutoRepositoryImpl.java
+++ src/main/java/com/tecdes/pedido/repository/ProdutoRepositoryImpl.java
@@ -1,5 +1,24 @@
 package com.tecdes.pedido.repository;
 
-public class ProdutoRepositoryImpl {
+import java.util.List;
 
+import com.tecdes.pedido.Model.entity.Produto;
+
+
+
+// Implementação que usa o DAO
+public class ProdutoRepositoryImpl implements ProdutoRepository{
+    
+    private final MotoristaDAO motoristaDAO = new MotoristaDAO();
+
+
+    @Override
+    public List<Produto> buscarTodos() {
+        return produtoDAO.buscarTodos(); 
+    }
+
+    @Override
+    public void salvar(Produto produto) {
+        
+    }
 }
