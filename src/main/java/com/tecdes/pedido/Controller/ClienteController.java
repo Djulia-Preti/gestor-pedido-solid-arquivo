ee0f6b79f4414eef1d0e030341034160ffa3c30c
djuliapacher-spec <djulia_pacher@estudante.sc.senai.br>
2025-11-05 21:41:40 -0300

    Coloquei em cada pasta de cliente
    Coloquei os códigos em cada pasta de cliente

--- src/main/java/com/tecdes/pedido/Controller/ClienteController.java
+++ src/main/java/com/tecdes/pedido/Controller/ClienteController.java
@@ -1,5 +1,19 @@
 package com.tecdes.pedido.controller;
 
+import java.util.List;
+
+import com.tecdes.pedido.modelo.entiny.Cliente;
+import com.tecdes.pedido.service.PedidoService;
+
 public class ClienteController {
     
+    private final PedidoService service = new PedidoService();
+
+    public void salvar(String nome, String fone) {
+        service.salvarCliente(nome, fone);
+    }
+
+    public List<Cliente> listarTodos() {
+        return service.buscarTodos();
+    }
 }
--- src/main/java/com/tecdes/pedido/View/ClienteView.java
+++ src/main/java/com/tecdes/pedido/View/ClienteView.java
@@ -1,5 +1,42 @@
 package com.tecdes.pedido.view;
 
-public class ClienteView {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableColumnModel;
+
+import com.tecdes.pedido.controller.ClienteController;
+import com.tecdes.pedido.entity.Cliente;
+
+public class ClienteView extends JFrame{
+    
+    private final ClienteController controller = new ClienteController();
+
+    private JTextField txtNome;
+    private JTextField txtFone;
+    private JButton btnSalvar;
+
+public ClienteView() {
+    super("Cadastro de Pedidos - Lanchonete App");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new BorderLayout(10, 10));
+
+    setupCadastroPainel();
+
+    setupListagemPainel();
+
+    pack();
+    setLocationRelativeTo(null);
+  }
+
+  private 
 }
